cd82a6fb53170c858234a08b91d4097e7c987228
DiPokemon <dimitrnklnk@gmail.com>
2022-11-27 03:19:37 +0300

    add CF7

--- views/replace_shortcode.php
+++ views/replace_shortcode.php
@@ -2,7 +2,7 @@
 	<div class="main_slider-wrapper">  
       <?php foreach ( self::$model->get_list() as $item ): $length++ ?>
         <div class="main_slide-item" style="background-image: url('<?= self::$model->get_image_attachment_filepath($item->image_attachment_id) ?>')">
-            <div class="main_slide-body container">
+            <div class="main_slide-body">
                 <div class="main_slide-left">
                     <div class="main_slide-title"><h2><?= $item->title ?></h2></div>
                     <div class="main_slide-subtitle"><?= $item->subtitle ?></div>
@@ -10,7 +10,7 @@
                     <a class="main_slide-btn btn" href="<?= $item->link ?>">Подробнее</a>
                 </div>
                 <div class="main_slide-right">
-                    Форма связи
+                    <?php echo do_shortcode('[contact-form-7 id="54" title="Контактная форма 1"]'); ?>
                 </div>
             </div>            
         </div>
